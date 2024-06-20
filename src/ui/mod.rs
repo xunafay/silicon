fb7ff116ec8922692935ae0f41dbe1f47b39736a
xunafay <me@xunafay.io>
2024-06-20 20:20:39 +0200

    fix: start inspector ui collapsed

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -46,17 +46,21 @@ fn inspector_ui(world: &mut World) {
     };
     let mut egui_context = egui_context.clone();
 
-    egui::Window::new("UI").show(egui_context.get_mut(), |ui| {
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            // equivalent to `WorldInspectorPlugin`
-            bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
+    egui::Window::new("Inspector")
+        .default_open(false)
+        .show(egui_context.get_mut(), |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                // equivalent to `WorldInspectorPlugin`
+                bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
 
-            egui::CollapsingHeader::new("Materials").show(ui, |ui| {
-                bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(world, ui);
+                egui::CollapsingHeader::new("Materials").show(ui, |ui| {
+                    bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(
+                        world, ui,
+                    );
+                });
+
+                ui.heading("Entities");
+                bevy_inspector_egui::bevy_inspector::ui_for_world_entities(world, ui);
             });
-
-            ui.heading("Entities");
-            bevy_inspector_egui::bevy_inspector::ui_for_world_entities(world, ui);
         });
-    });
 }
